4ef89ca8024c8752dd81a04ae4e41e87d7c61c00
shifas <sshifas333@gmail.com>
2024-07-09 16:25:06 +0530

    first commit

---
+++ js/new1.js
@@ -0,0 +1,20 @@
+// console.log("have a good day");
+// /console.log is used to display out put/ 
+// a=52;
+// a%2==0?console.log(a+"is even number"):console.log(a+"is odd number");
+a=60;
+b=40;
+c=20;
+if(a>=b&&a>=c)
+{
+    console.log(" a is greater");
+}
+else if(b>=a&&b>=c)
+{
+    console.log(" b is grreater");
+}
+else
+{
+    console.log("c is greater");
+
+}
---
+++ js/operation.js
@@ -0,0 +1,35 @@
+m=100;
+if(m>95)
+{
+console.log("A+grade");
+}
+else if(m>90)
+{
+    console.log("A");
+}
+else if(m>85)
+{
+    console.log("b+grade");
+
+}
+else if(m>80)
+{
+    console.log("b");
+}
+else if(m>75)
+{
+    console.log("c+ grade");
+}
+else if(m>70)
+{
+    console.log("c");
+}
+else if(m>65)
+{
+    console.log("d");
+}
+else
+{
+
+    console.log("fail");
+}
