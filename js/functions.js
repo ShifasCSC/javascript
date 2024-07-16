9719f83f2cd0267d7ceff3e44d0985995765ea5c
shifas <sshifas333@gmail.com>
2024-07-16 15:59:54 +0530

    new file added old file updated

---
+++ js/functions.js
@@ -0,0 +1,109 @@
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+// console.log("good morning");
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+// console.log("good after-noon");
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+// console.log("good evening");
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+// console.log("good night");
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+
+// usage of fuction
+
+// function psd() {
+// console.log("+++++++++++++++++++");
+// console.log("-------------------");
+// console.log("xxxxxxxxxxxxxxxxxxx");
+// console.log("*******************");
+    
+// }
+// console.log("good morning");
+// psd();
+// console.log("good after-noon");
+// psd();
+// console.log("good evening");
+// psd();
+// console.log("good night");
+// psd();
+
+// creation of function
+
+//FUNCTION WITH ARGUMENT--------------------
+//{
+// function eatfood(money)// ivde bracket-il kodthathanu parameter,,parameter eppozhum function create cheyyumbolanu parameter kodkkuka,,,function call cheyyumbol ath argument
+//  {
+//     console.log("i will eat food for" +money+ "from hotel");
+// }
+// eatfood(250);
+
+//ippol call cheythappo nammal 250 kodthathanu argument
+//}--------
+
+// FUNCTION WITH RETURN{-----
+
+// which return the data when the function is called
+
+// function buyEggs() {
+//     {
+//         return "buy 10 eggs from the store";
+//     }
+
+// }
+// x=buyEggs()//or console.log(buyEgggs());
+// console.log(x);}---
+
+//FUNCTION WITH ARGUMENT AND RETURN----------------------------------------
+
+// function buyEgggs(money) {
+//     return "buy Eggs at" +money;
+    
+// }
+// console.log(buyEgggs);//reference ->it prints the whole return datas inside the function 
+// console.log(buyEgggs(100));
+//------------------------------------------------------------------------------------
+
+
+
+//ANONYMOUS FUNCTION----------------------IT NOT HAVE A SPECIFIC FUN_NAME-----------------------
+//in anonymous function we assign that whole functio into a variable here " x " is the variable
+// x=function(num) {
+//     console.log("hello"+num);
+    
+// }
+// x(5);
+//--------------------------------------------------------------------------------------------------------------------------
+
+
+//ARROW FUNCTION-------------------------
+// x=(a)=>
+//     {
+//         console.log("hi"+a);
+
+// }
+// x(6);
+
+
+//write an EXAMPLE with OF FUNCTION ARGUMENT print an multiplication table
+
+
+// function table(n) {
+//     for(i=1;i<=n;i++)
+//     console.log(i+"x"+n+"="+i*n);
+    
+// }
+// table(5);
---
+++ js/let,var.js
@@ -0,0 +1,14 @@
+function asd() {
+    if (true) 
+        {
+            console.log(a);
+            
+        }
+        console.log(a);
+        
+        let a=20; 
+        
+    }
+    console.log(a);
+    asd()
+    console.log(a);
