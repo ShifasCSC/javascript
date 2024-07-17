f43bc109e878bac07256778d0f2784074cce01a5
shifas <sshifas333@gmail.com>
2024-07-17 16:21:38 +0530

    new file added

---
+++ js/array.js
@@ -0,0 +1,51 @@
+// a=["hello",1,true,null,"hai",2,false,"day",null]
+// console.log(a);
+// console.log(a[7]);
+// console.log(a.length);
+// a.push("item")
+// console.log(a.join(" "));
+// console.log(a.indexOf(true));
+// console.log(a);
+// a.pop()//to delete the last item from the array
+// console.log(a);
+// a.shift()//to delete first item from the array
+// console.log(a);
+// a.splice(a.indexOf(false),1)//to delete data from the centre of the array
+// console.log(a);
+
+// //creating an empty array adn adding data into it
+
+// b=[]
+
+// b.push("hello")
+// console.log(b);
+
+
+//PRINT FIZZ BUZZ BY 
+// console.log("to push fizz buzz into an array");
+// n=[]
+// num=prompt("enter a number")
+// console.log("you are entered"+num);
+// for(i=1;i<=num;i++)
+// {
+//     if (i%3==0 && i%5==0) 
+//     {
+//         n.push("fizzbuzz");
+    
+//     } 
+//     else if (i%5==0)
+//     {
+//         n.push("buzz");
+                                             /////this fizz buzz code is not correct////
+//     } 
+//    else if(i%3==0)
+//     {
+//         n.push("fizz");
+//     }
+//    else 
+//     {
+//         n.push(i);
+    
+//     }
+// }
+// console.log(n);
