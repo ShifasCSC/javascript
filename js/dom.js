7832e68a727cd0ed8753cd91c96c75707fcb64bd
shifas <sshifas333@gmail.com>
2024-07-23 13:41:14 +0530

    new file added

---
+++ js/dom.js
@@ -0,0 +1,10 @@
+// document.getElementById("main").innerHTML="<h1>Hello team<h1>"
+// document.getElementById("main").innerText="<h1>Hello team<h1>"  
+// document.getElementById("main").textContent="<h1>Hello team<h1>" 
+let tag=document.createElement("h1")
+tag.textContent="helo"
+document.getElementById("asd").style.backgroundColor="red"
+document.getElementById("asd").appendChild(tag)
+// document.getElementById("asd").removeChild(tag)
+
+
